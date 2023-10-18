31d07249c379d2834a02aa710a2247a4cd8b8808
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-18 15:48:33 -0400

    updates public folder

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,45 +17,45 @@ router.get("/", async (req, res) => {
   }
 });
 
-// GET one gallery
-router.get("/gallery/:id", async (req, res) => {
-  try {
-    const dbGalleryData = await Gallery.findByPk(req.params.id, {
-      include: [
-        {
-          model: Painting,
-          attributes: [
-            "id",
-            "title",
-            "artist",
-            "exhibition_date",
-            "filename",
-            "description",
-          ],
-        },
-      ],
-    });
+// GET one post
+// router.get("/gallery/:id", async (req, res) => {
+//   try {
+//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Painting,
+//           attributes: [
+//             "id",
+//             "title",
+//             "artist",
+//             "exhibition_date",
+//             "filename",
+//             "description",
+//           ],
+//         },
+//       ],
+//     });
 
-    const gallery = dbGalleryData.get({ plain: true });
-    res.render("gallery", { gallery });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     const gallery = dbGalleryData.get({ plain: true });
+//     res.render("gallery", { gallery });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
-// GET one painting
-router.get("/painting/:id", async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
+// // GET one painting
+// router.get("/painting/:id", async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
 
-    const painting = dbPaintingData.get({ plain: true });
+//     const painting = dbPaintingData.get({ plain: true });
 
-    res.render("painting", { painting });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     res.render("painting", { painting });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
