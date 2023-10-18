5c6704a750e761908a9e9b9a049f8b27e6abd01b
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-18 16:04:26 -0400

    fixes server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const path = require("path");
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
