5a569b93aed892046cdc298001f7c4de6ae075d1
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-19 21:51:37 -0400

    adds index to models

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,9 @@ const sess = {
   resave: false,
   saveUninitialized: true,
 };
+
+// Configure express-handlebars
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -24,6 +28,11 @@ app.use(session(sess));
 
 // turn on routes
 app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
